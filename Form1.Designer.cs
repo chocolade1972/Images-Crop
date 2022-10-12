204350384650b80abc3e3eae37204cc43ee36a0e
chocolade1972 <30846123+chocolade1972@users.noreply.github.com>
2022-10-12 15:49:43 +0300

    Crop Images

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Image_Crop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBoxSaveRectangles = new System.Windows.Forms.CheckBox();
+            this.checkBoxClearRectangles = new System.Windows.Forms.CheckBox();
+            this.checkBoxDrawBorder = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 323);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(658, 323);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1203, 323);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(212, 511);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBoxSaveRectangles
+            // 
+            this.checkBoxSaveRectangles.AutoSize = true;
+            this.checkBoxSaveRectangles.Location = new System.Drawing.Point(9, 238);
+            this.checkBoxSaveRectangles.Name = "checkBoxSaveRectangles";
+            this.checkBoxSaveRectangles.Size = new System.Drawing.Size(149, 24);
+            this.checkBoxSaveRectangles.TabIndex = 5;
+            this.checkBoxSaveRectangles.Text = "Save Rectangles";
+            this.checkBoxSaveRectangles.UseVisualStyleBackColor = true;
+            this.checkBoxSaveRectangles.CheckedChanged += new System.EventHandler(this.checkBoxSaveRectangles_CheckedChanged);
+            // 
+            // checkBoxClearRectangles
+            // 
+            this.checkBoxClearRectangles.AutoSize = true;
+            this.checkBoxClearRectangles.Location = new System.Drawing.Point(9, 132);
+            this.checkBoxClearRectangles.Name = "checkBoxClearRectangles";
+            this.checkBoxClearRectangles.Size = new System.Drawing.Size(150, 24);
+            this.checkBoxClearRectangles.TabIndex = 6;
+            this.checkBoxClearRectangles.Text = "Clear Rectangles";
+            this.checkBoxClearRectangles.UseVisualStyleBackColor = true;
+            this.checkBoxClearRectangles.CheckedChanged += new System.EventHandler(this.checkBoxClearRectangles_CheckedChanged);
+            // 
+            // checkBoxDrawBorder
+            // 
+            this.checkBoxDrawBorder.AutoSize = true;
+            this.checkBoxDrawBorder.Location = new System.Drawing.Point(9, 185);
+            this.checkBoxDrawBorder.Name = "checkBoxDrawBorder";
+            this.checkBoxDrawBorder.Size = new System.Drawing.Size(117, 24);
+            this.checkBoxDrawBorder.TabIndex = 7;
+            this.checkBoxDrawBorder.Text = "Draw Border";
+            this.checkBoxDrawBorder.UseVisualStyleBackColor = true;
+            this.checkBoxDrawBorder.CheckedChanged += new System.EventHandler(this.checkBoxDrawBorder_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(268, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.checkBoxSaveRectangles);
+            this.groupBox1.Controls.Add(this.checkBoxDrawBorder);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.checkBoxClearRectangles);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(512, 275);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(945, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number Of Rectangles";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1119, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(654, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Image To Crop";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 30);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Image To Crop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(283, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 30);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Cropped Images Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(268, 26);
+            this.textBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1427, 846);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBoxSaveRectangles;
+        private System.Windows.Forms.CheckBox checkBoxClearRectangles;
+        private System.Windows.Forms.CheckBox checkBoxDrawBorder;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,394 @@
+﻿using Newtonsoft.Json;
+using Ookii.Dialogs.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace Image_Crop
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rect;
+        int pixelsCounter = 0;
+        Color SelectedColor = Color.LightGreen;
+        List<DrawingRectangle> DrawingRects = new List<DrawingRectangle>();
+        Bitmap rectImage;
+        int saveRectanglesCounter = 1;
+        bool drawBorder = true;
+        bool clearRectangles = true;
+        bool saveRectangles = true;
+        //bool drawnNewRectangle = false; // To think how to work with this flag bool variable
+        // where to use it how and how to solce this problem !!!!!
+        string rectangleName;
+        Dictionary<string, string> FileList = new Dictionary<string, string>();
+        string selectedPath;
+        int x, y;
+        bool addToListBox = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //LoadFile(textBox1, )
+
+            textBox2.Text = Properties.Settings.Default.CroppedImagesFolder;
+            selectedPath = textBox2.Text;
+
+            if (textBox1.Text != "")
+            {
+                pictureBox2.Image = System.Drawing.Image.FromFile(textBox1.Text);
+            }
+            
+            checkBoxDrawBorder.Checked = true;
+            checkBoxClearRectangles.Checked = true;
+            checkBoxSaveRectangles.Checked = true;
+
+            // To make here loadfile and use other places the savefile !!!!!
+            // or even before in start of constructor to use loadfile ? !!!!!
+
+            if (selectedPath != "" && selectedPath != null)
+            {
+                if (System.IO.File.Exists(Path.Combine(selectedPath, "rectangles.txt")))
+                {
+                    string g = System.IO.File.ReadAllText(Path.Combine(selectedPath, "rectangles.txt"));
+                    FileList = JsonConvert.DeserializeObject<Dictionary<string, string>>(g);
+                    listBox1.DataSource = FileList.ToList();
+                }
+            }
+
+            int counter = 1;
+            for(int i = 2; i < 79; i++)
+            {
+                counter++;
+            }
+        }
+
+        public class DrawingRectangle
+        {
+            public Rectangle Rect => new Rectangle(Location, Size);
+            public Size Size { get; set; }
+            public Point Location { get; set; }
+            public Control Owner { get; set; }
+            public Point StartPosition { get; set; }
+            public Color DrawingcColor { get; set; } = Color.LightGreen;
+            public float PenSize { get; set; } = 3f;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+
+            //addToListBox = true;
+
+            if (pictureBox2.Image != null && selectedPath != null)
+            {
+                DrawingRects.Add(new DrawingRectangle()
+                {
+                    Location = e.Location,
+                    Size = Size.Empty,
+                    StartPosition = e.Location,
+                    Owner = (Control)sender,
+                    DrawingcColor = SelectedColor // <= Shape's Border Color
+                });
+            }
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            int X = e.X;
+            int Y = e.Y;
+
+            if (e.Button != MouseButtons.Left) return;
+
+            if ((X >= 0 && X <= pictureBox2.Width) && (Y >= 0 && Y <= pictureBox2.Height))
+            {
+                if (pictureBox2.Image != null && selectedPath != null)
+                {
+                    x = e.X;
+                    y = e.Y;
+
+                    var dr = DrawingRects[DrawingRects.Count - 1];
+                    if (e.Y < dr.StartPosition.Y) { dr.Location = new Point(dr.Rect.Location.X, e.Y); }
+                    if (e.X < dr.StartPosition.X) { dr.Location = new Point(e.X, dr.Rect.Location.Y); }
+
+                    dr.Size = new Size(Math.Abs(dr.StartPosition.X - e.X), Math.Abs(dr.StartPosition.Y - e.Y));
+                    pictureBox2.Invalidate();
+                }
+            }
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+
+            //if (addToListBox == false) return;
+
+            if (DrawingRects.Count > 0 && pictureBox2.Image != null && selectedPath != null)
+            {
+                // In the mouseup evebt here to make it will save and add information to
+                // the listBox only if trying to draw inside the image in the
+                // picturebox2 if the image is smaller then the pictureBox2.
+                // like i did with this line in the pictureBox2 paint event :
+                // if ((x >= 0 && x <= pictureBox2.Image.Size.Width) && (y >= 0 && y <= pictureBox2.Image.Size.Height))
+                // maybe to use a flag in the pictureBox paint event so this line will affect also here
+                // in the mouse move events and maybe if needed also in other mouse events too !!!!!
+
+                // The last drawn shape
+                var dr = DrawingRects.Last();
+                if (dr.Rect.Width > 0 && dr.Rect.Height > 0)
+                {
+                    // Not working good yet with the addToListBox flag.
+                    // to check why here in this event mouseup and other mouse event.
+                    // and in the pictureBox2 paint event. !!!!!
+
+                    rectImage = cropAtRect((Bitmap)pictureBox2.Image, dr.Rect);
+                    if (saveRectangles)
+                    {
+
+
+                        rectangleName = GetNextName(Path.Combine(selectedPath, "Rectangle"), ".bmp");
+
+                        //rectangleName = @"d:\Rectangles\rectangle" + saveRectanglesCounter + ".bmp";
+                        FileList.Add($"{dr.Location}, {dr.Size}", rectangleName);
+                        string json = JsonConvert.SerializeObject(
+    FileList,
+    Formatting.Indented // this for pretty print
+);
+                        using (StreamWriter sw = new StreamWriter(Path.Combine(selectedPath, "rectangles.txt"), false))
+                        {
+                            sw.Write(json);
+                            sw.Close();
+                        }
+
+                        rectImage.Save(rectangleName);
+                        saveRectanglesCounter++;
+                    }
+                    pixelsCounter = rect.Width * rect.Height;
+
+                    pictureBox1.Invalidate();
+
+                    listBox1.DataSource = FileList.ToList();
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+
+                    pictureBox2.Focus();
+                    Graphics g = Graphics.FromImage(this.pictureBox1.Image);
+                    g.Clear(this.pictureBox1.BackColor);
+                }
+            }
+        }
+
+        string GetNextName(string baseName, string extension)
+        {
+            int counter = 1;
+            string nextName = baseName + counter + extension;
+            while (System.IO.File.Exists(nextName))
+            {
+                counter++;
+                nextName = baseName + counter + extension;
+            }
+            return nextName;
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, pictureBox2.ClientRectangle, Color.Red, ButtonBorderStyle.Solid);
+
+            if (pictureBox2.Image != null && selectedPath != null)
+            {
+                if ((x >= 0 && x <= pictureBox2.Image.Size.Width) && (y >= 0 && y <= pictureBox2.Image.Size.Height))
+                {
+                    //addToListBox = true;
+
+                    DrawShapes(e.Graphics);
+                }
+                else
+                {
+                    //addToListBox = false;
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (drawBorder)
+            {
+                ControlPaint.DrawBorder(e.Graphics, pictureBox1.ClientRectangle, Color.Red, ButtonBorderStyle.Solid);
+            }
+
+            //if (addToListBox == false) return;
+
+            if (rectImage != null && DrawingRects.Count > 0)
+            {
+                var dr = DrawingRects.Last();
+                e.Graphics.DrawImage(rectImage, dr.Rect);
+
+                if (clearRectangles)
+                {
+                    DrawingRects.Clear();
+                    pictureBox2.Invalidate();
+                }
+            }
+        }
+
+        private void DrawShapes(Graphics g)
+        {
+            if (DrawingRects.Count == 0) return;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            foreach (var dr in DrawingRects)
+            {
+                using (Pen pen = new Pen(dr.DrawingcColor, dr.PenSize))
+                {
+                    g.DrawRectangle(pen, dr.Rect);
+                };
+            }
+        }
+
+        public Bitmap cropAtRect(Bitmap b, Rectangle r)
+        {
+            Bitmap nb = new Bitmap(r.Width, r.Height);
+            using (Graphics g = Graphics.FromImage(nb))
+            {
+                g.DrawImage(b, -r.X, -r.Y);
+                return nb;
+            }
+        }
+
+        private void checkBoxDrawBorder_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxDrawBorder.Checked)
+            {
+                drawBorder = true;
+            }
+            else
+            {
+                drawBorder = false;
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void checkBoxClearRectangles_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxClearRectangles.Checked)
+            {
+                clearRectangles = true;
+            }
+            else
+            {
+                clearRectangles = false;
+            }
+
+            pictureBox2.Invalidate();
+        }
+
+        private void checkBoxSaveRectangles_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = ((ListBox)sender).SelectedItem;
+            var itemCast = (KeyValuePair<string, string>)item;
+            pictureBox1.Image = System.Drawing.Image.FromFile(itemCast.Value);
+
+            // Here to make that it will show the cropped image in pictureBox1
+            // in the position where it was taken in pictureBox2
+            // and not like now that its showing it in position 0,0 in pictureBox1.
+            // !!!!!
+        }
+
+        private void lblImageToCrop_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+        }
+
+        private void SaveFile(string contentToSave, string fileName)
+        {
+            string applicationPath = Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory); // the directory that your program is installed in
+            string saveFilePath = Path.Combine(applicationPath, fileName);
+            File.WriteAllText(saveFilePath, contentToSave);
+        }
+
+        private void LoadFile(TextBox loadTo, string fileName)
+        {
+            string applicationPath = Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory); // the directory that your program is installed in
+            string saveFilePath = Path.Combine(applicationPath, fileName); // add a file name to this path.  This is your full file path.
+
+            if (File.Exists(saveFilePath))
+            {
+                loadTo.Text = File.ReadAllText(saveFilePath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VistaOpenFileDialog dialog = new VistaOpenFileDialog();
+            {
+                dialog.Filter = "JPG,BMP|*.jpg;*.bmp";
+            };
+            
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+
+                Bitmap bitmap = new Bitmap(dialog.FileName);
+                if(bitmap.Size != new Size(512,512))
+                {
+                    //var size = pictureBox2.Image.Size;
+                    //pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                else
+                {
+                    //var size = pictureBox2.Image.Size;
+                    //pictureBox2.SizeMode = PictureBoxSizeMode.Normal;
+                }
+
+                pictureBox2.Image = Image.FromFile(dialog.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = dialog.SelectedPath;
+                selectedPath = dialog.SelectedPath;
+
+                Properties.Settings.Default.CroppedImagesFolder = selectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
+
+// Still i can draw a rectangle with height 0 !!!!! to check why and to check if i can draw
+// also a rectangle when width is 0. i should not be able to draw rectangle when width or 
+// height are 0 if both or one of them !!!!! 
+
+// To draw border in red around each pictureBox1 and 2 to see the pictureBoxes edges !!!!!
+// also to limit the mouse movement to be only in the pictureBox are from the inside.
+// now i can draw inside the pictureBox2 out(inside) !!!!!
